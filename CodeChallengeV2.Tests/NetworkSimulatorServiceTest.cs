b4da7c97319e7d46f3f2d903e80d5299723bacc6
Robozou <viktor.horsted@gmail.com>
2019-04-16 01:41:17 +0200

    Added more NetworkSimTests

--- CodeChallengeV2.Tests/NetworkSimulatorServiceTest.cs
+++ CodeChallengeV2.Tests/NetworkSimulatorServiceTest.cs
@@ -12,7 +12,7 @@ namespace CodeChallengeV2.Tests
     public class NetworkSimulatorServiceTest
     {
         [Theory]
-        [JsonFileData("graph1.json")]        
+        [JsonFileData("graph1.json")]
         public async void FindCritialGateways(NetworkGraph graph)
         {
             var res = await new NetworkSimulatorService().FindCritialGateways(graph);
@@ -27,6 +27,30 @@ namespace CodeChallengeV2.Tests
             var res = await new NetworkSimulatorService().FindCritialGateways(graph);
 
             Assert.Null(res.FirstOrDefault());
+        }
+
+        [Theory]
+        [JsonFileData("graph3.json")]
+        public async void FindCritialGateways3(NetworkGraph graph)
+        {
+            var res = await new NetworkSimulatorService().FindCritialGateways(graph);
+            foreach (Node n in res)
+            {
+                Console.WriteLine(n.ToString());
+            }
+            Assert.Equal(2, res.Count);
+        }
+
+        [Theory]
+        [JsonFileData("graph4.json")]
+        public async void FindCritialGateways4(NetworkGraph graph)
+        {
+            var res = await new NetworkSimulatorService().FindCritialGateways(graph);
+            foreach (Node n in res)
+            {
+                Console.WriteLine(n.ToString());
+            }
+            Assert.Equal(2, res.Count);
         }
     }
 }
