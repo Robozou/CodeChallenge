d535777e4f2af43234f8fb6efc2c70d118ec6a57
Robozou <14161475+Robozou@users.noreply.github.com>
2019-04-16 02:55:44 +0200

    Geocode done

--- CodeChallengeV2/Services/GeocodeService.cs
+++ CodeChallengeV2/Services/GeocodeService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using CodeChallengeV2.Models;
 
@@ -8,9 +9,12 @@ namespace CodeChallengeV2.Services
 {
     public class GeocodeService : IGeocodeService
     {
-        public Task<string> FindCountryCode(GeocodePayload payload)
+        public async Task<string> FindCountryCode(GeocodePayload payload)
         {
-            return Task.FromResult("DK");
+            HttpClient httpClient = new HttpClient();
+            string request = $"https://geocode.xyz/{payload.Lat},{payload.Long}?json=1&auth=637940924585023510678x2173";
+            HttpResponseMessage httpResponseMessage = (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
+            return ((string)JObject.Parse(await httpResponseMessage.Content.ReadAsStringAsync())["state"]).ToUpper();
         }
     }
 }
